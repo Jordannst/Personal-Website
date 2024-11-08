7484091fc77d1bc6fb78ab98afedb07f0001ee38
Jordannst <s22310272@student.unklab.ac.id>
2024-11-09 00:21:00 +0800

    Modified Landing Page

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -4,21 +4,25 @@ const LandingPage = () => {
       <div className="blob" />
       <div className="text-content">
         <article id="hello-friend">
-          <p>Hello Saya,</p>
+          <p>Hello I&apos;m,</p>
         </article>
+
         <article id="name">
           <p>Jordan Sutarto</p>
         </article>
+
         <article id="work">
           <p>I Design &amp; Code For Web</p>
         </article>
+
         <p id="info-para">
-          Web Developer with experience of Digital Concept Arts, Frontend Web
-          Designs. I Love fun Web UI, collaboration and making products.
+          Second year student interested in Digital Concept Art, Frontend Web
+          Design. I love fun Web UIs, collaboration, and creating products.
           <br />
           <br />I value simple content structure, clean design patterns, and
           thoughtful interactions.
         </p>
+
         <div className="contact-btn-div">
           <a href="mailto:jordansutart08@gmail.com" tabIndex={-1}>
             <button className="letsTalkBtn">
